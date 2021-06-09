1e79a3109260aee5f420e37ed25c2311ff0a1c16
JDaniel <daniel.jose.45@hotmail.com>
2021-06-09 08:59:31 -0500

    pj creation

---
+++ src/test/java/com/insoftar/form/FormularioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.insoftar.form;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FormularioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
